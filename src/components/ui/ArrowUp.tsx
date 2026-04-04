591cba2222508cc584b7a1dbd816213a2a4a071c
rohit dev <rohit@visiontech.world>
2026-04-04 15:13:27 +0530

    image change

--- src/components/ui/ArrowUp.tsx
+++ src/components/ui/ArrowUp.tsx
@@ -9,7 +9,7 @@ const ArrowUpButton = () => {
     };
 
     return (
-        <div className='fixed bottom-5 left-1/2 -translate-x-1/2 md:bottom-10 z-30'>
+        <div className='fixed bottom-8 right-5 md:bottom-10 md:right-10 z-50'>
             <button
                 onClick={scrollToTop}
                 className="w-10 h-10 bg-[#2b4261] rounded-full flex items-center justify-center text-white hover:bg-[#31527d] transition-colors shadow-lg"
